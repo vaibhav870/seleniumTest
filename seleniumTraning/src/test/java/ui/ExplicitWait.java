5100b7eb8092a8e442fe70c7d61ace4b4eb5aeb3
vaibhav <vaibhav@10.10.229.11>
2020-12-04 10:47:34 +0530

    Explicit Wait

--- seleniumTraning/src/test/java/ui/ExplicitWait.java
+++ seleniumTraning/src/test/java/ui/ExplicitWait.java
@@ -1,30 +1,35 @@
 package ui;
 
+import java.time.Duration;
 import java.util.concurrent.TimeUnit;
 
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.interactions.Actions;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
 
 import io.github.bonigarcia.wdm.WebDriverManager;
 
-public class Wait {
+public class ExplicitWait {
 
-	public static void main(String[] args) throws InterruptedException {
+	public static void main(String[] args) {
 		WebDriverManager.chromedriver().setup();
 		ChromeDriver driver = new ChromeDriver();
 		
 		driver.get("https://www.ebay.com/");
 		driver.manage().window().maximize();
-		driver.manage().timeouts().implicitlyWait(5, TimeUnit.SECONDS);
+	//	driver.manage().timeouts().implicitlyWait(5, TimeUnit.SECONDS);
 		WebElement elect=driver.findElement(By.xpath("//*[@id=\"mainContent\"]/div[1]/ul/li[5]/a"));
 		Actions mhover=new Actions(driver);
 		mhover.moveToElement(elect).perform();
 		//Thread.sleep(2000);
 		
-		driver.findElement(By.xpath("//*[@id=\"mainContent\"]/div[1]/ul/li[5]/div[2]/div[1]/div[2]/ul/li[2]/a")).click();
+		WebElement smartwatch= driver.findElement(By.xpath("//*[@id=\"mainContent\"]/div[1]/ul/li[5]/div[2]/div[1]/div[2]/ul/li[2]/a"));
 		
+		WebDriverWait wait= new WebDriverWait(driver,Duration.ofSeconds(10));
+		wait.until(ExpectedConditions.visibilityOf(smartwatch)).click();
 		
 	}
 
