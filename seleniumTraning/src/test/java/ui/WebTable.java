cf38939b1a61f764145757deaaf2090f7ed7f381
vaibhav <vaibhav@10.10.229.11>
2020-12-04 11:26:50 +0530

    web table

--- seleniumTraning/src/test/java/ui/WebTable.java
+++ seleniumTraning/src/test/java/ui/WebTable.java
@@ -1,10 +1,18 @@
 package ui;
 
+import org.openqa.selenium.chrome.ChromeDriver;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+
 public class WebTable {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		WebDriverManager.chromedriver().setup();
+		ChromeDriver driver = new ChromeDriver();
+		
+		driver.get("https://www.ebay.com/");
+		
+		Web
 	}
 
 }
